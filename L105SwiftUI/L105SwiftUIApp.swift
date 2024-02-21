39c635cd6d264b46e1d15c6b6ea0c2dc278d2810
pranay-L53 <pranay.prajapati@auxanoglobalservices.com>
2024-02-21 10:45:34 +0530

    1

---
+++ L105SwiftUI/L105SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  L105SwiftUIApp.swift
+//  L105SwiftUI
+//
+//  Created by Auxano on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct L105SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
