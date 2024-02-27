0ee1145bcbed989235914f0090f20c5f44c06fac
pranay-L53 <pranay.prajapati@auxanoglobalservices.com>
2024-02-27 13:43:30 +0530

    1343

---
+++ PlayGround/PranayPlayground.playground/Pages/RegularExpressions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+///: [Previous](@previous)
+
+import Foundation
+
+var test1 = "today is Sunny, the pets are playing outside."
+
+print(test1.range(of: "the"))
+print(test1.replacing("is", with: ":"))
+//print(test1.trimmingPrefix(while: "today"))
+
+print("==========")
+
+
+//print(test1.range(of: /[a-x]ay/))
+//print(test1.replacing("is", with: ":"))
+//print(test1.trimPrefix(while: "today"))
+
+print("==========")
+
+
+do {
+    //let testRegex = try Rregx()
+}
+
+print("==========")
+
+///: [Next](@next)
